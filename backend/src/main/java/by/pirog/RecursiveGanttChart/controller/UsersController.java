aa27256d494861c9206c4f3e6d3ee1d93416bcbd
sta66y <sta66y@yahoo.com>
2025-11-20 16:07:20 +0700

    UsersController

--- backend/src/main/java/by/pirog/RecursiveGanttChart/controller/UsersController.java
+++ backend/src/main/java/by/pirog/RecursiveGanttChart/controller/UsersController.java
@@ -1,4 +1,43 @@
 package by.pirog.RecursiveGanttChart.controller;
 
+import by.pirog.RecursiveGanttChart.dto.UserDto;
+import by.pirog.RecursiveGanttChart.service.UsersService;
+import by.pirog.RecursiveGanttChart.storage.enums.UserRoles;
+import by.pirog.RecursiveGanttChart.storage.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UsersController {
+
+    private final UsersService userService;
+
+    @PostMapping("/projects/{projectId}/users/{userId}/role/{role}")
+    public void assignRoleToUser(
+            @PathVariable Integer projectId,
+            @PathVariable Integer userId,
+            @PathVariable UserRoles role,
+            @AuthenticationPrincipal User currentUser) {
+        userService.assignRoleToUser(projectId, userId, role, currentUser);
+    }
+
+    @GetMapping("/projects/{projectId}/users")
+    public List<UserDto> getProjectUsers(
+            @PathVariable Integer projectId,
+            @AuthenticationPrincipal User currentUser) {
+        return userService.getProjectUsers(projectId, currentUser);
+    }
+
+    @DeleteMapping("/projects/{projectId}/users/{userId}")
+    public void removeUserFromProject(
+            @PathVariable Integer projectId,
+            @PathVariable Integer userId,
+            @AuthenticationPrincipal User currentUser) { // Добавил currentUser
+        userService.removeUserFromProject(projectId, userId, currentUser);
+    }
 }
