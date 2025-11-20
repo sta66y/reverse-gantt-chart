8e46f0473fb4adcf470e027817cf0d5cfed69917
sta66y <sta66y@yahoo.com>
2025-11-20 16:06:56 +0700

    updated GlobalExceptionHandler

--- backend/src/main/java/by/pirog/RecursiveGanttChart/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/by/pirog/RecursiveGanttChart/controller/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package by.pirog.RecursiveGanttChart.controller;
 
 import by.pirog.RecursiveGanttChart.exception.ProjectNotFound;
+import by.pirog.RecursiveGanttChart.exception.UserNotFound;
 import by.pirog.RecursiveGanttChart.util.AppError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,10 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ProjectNotFound.class)
     public ResponseEntity<AppError> projectNotFound(ProjectNotFound ex) {
         return new ResponseEntity<>(new AppError(ex.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<AppError> userNotFound(UserNotFound ex) {
+        return new ResponseEntity<>(new AppError(ex.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<AppError> methodArgumentNotValid(MethodArgumentNotValidException ex) {
         String errorMessage = ex.getBindingResult()
@@ -26,6 +35,30 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(
                 new AppError(errorMessage, HttpStatus.BAD_REQUEST.value()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<AppError> securityException(SecurityException ex) {
+        return new ResponseEntity<>(
+                new AppError(ex.getMessage(), HttpStatus.FORBIDDEN.value()),
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<AppError> accessDeniedException(AccessDeniedException ex) {
+        return new ResponseEntity<>(
+                new AppError("Access denied", HttpStatus.FORBIDDEN.value()),
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<AppError> illegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(
+                new AppError(ex.getMessage(), HttpStatus.BAD_REQUEST.value()),
                 HttpStatus.BAD_REQUEST
         );
     }
